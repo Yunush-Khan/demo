f0abc297ba3560c7e89e1926ebf30eb653ecd492
Yunush-Khan <7yunushkhan@gmail.com>
2024-05-09 23:41:12 +0530

    spering-html

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+// nav menu style
+var nav = $("#navbarSupportedContent");
+var btn = $(".custom_menu-btn");
+btn.click
+btn.click(function (e) {
+
+    e.preventDefault();
+    nav.toggleClass("lg_nav-toggle");
+    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
+});
+
+
+function getCurrentYear() {
+    var d = new Date();
+    var currentYear = d.getFullYear()
+
+    $("#displayDate").html(currentYear);
+}
+
+getCurrentYear();
+
+var h_cont = document.getElementById ('best');
+
+h_cont.textContent = '<h2> hello are you ok </h2';
+
+
+var bgcolor = document.getElementsByName ('section');
+
+bgcolor.style.backgroundColor = '#ccc';
